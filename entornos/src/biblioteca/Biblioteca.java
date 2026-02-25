471b9b3db6951e6238369bd00745fc69d503d92d
Diurno <Diurno@pc0308>
2026-02-25 10:23:46 +0100

    Primer commit

---
+++ entornos/src/biblioteca/Biblioteca.java
@@ -0,0 +1,18 @@
+package biblioteca;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Libro> listaLibros = new ArrayList<>();
+
+    public void agregarLibro(Libro l) {
+        listaLibros.add(l);
+        System.out.println("Libro añadido con éxito.");
+    }
+
+    public void mostrarLibros() {
+        for (Libro l : listaLibros) {
+            System.out.println(l);
+        }
+    }
+}
---
+++ entornos/src/biblioteca/Libro.java
@@ -0,0 +1,16 @@
+package biblioteca;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro: " + titulo + " | Autor: " + autor;
+    }
+}
